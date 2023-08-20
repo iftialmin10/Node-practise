3f011f58c8817c2b9e638a15fe5c5c08b86e04ce
Ifti-Alamin <iftialmin10@gmail.com>
2023-08-21 00:52:22 +0600

    Folder 6 - file 4

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,11 @@
-console.log('Starting');
+const request = require('request');
 
-// setTimeout(function(){},timer)
-setTimeout(() => {
-  console.log('2 Sec Timer');
-}, 2000); // 2second = 2000 mili_sec
+const url =
+  'https://api.openweathermap.org/data/2.5/weather?lat=37.8267&lon=-122.4233&appid=df4190d5f6797b089117492ddd1e9dcb';
 
-setTimeout(() => {
-  console.log('0 Sec timer');
-}, 0);
-
-console.log('Stopping');
+//call request function with two argument, 1st is an option object which outline what we'd like to do, thats where we provide the url and other information. 2nd argument is a function to run once we actually have that response.
+request({ url: url }, (error, response) => {
+  //here error message shown when no internet or find any issue and response for output
+  const data = JSON.parse(response.body); // response is a string and we can find result json data file by parse data
+  console.log(data);
+}); //request({option_object: variable_name},function run and send response )
