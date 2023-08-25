6b302b0ee9aed49b16bd2ab2d4830c3bf51a8eb5
Ifti-Alamin <iftialmin10@gmail.com>
2023-08-25 16:17:03 +0600

    Removed

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,11 @@
 const request = require('request');
 
 const url =
-  'https://api.openweathermap.org/data/2.5/weather?lat=37.8267&lon=-122.4233&units=metric&appid=df4190d5f6797b089117492ddd1e9dcb';
+  'https://api.openweathermap.org/data/2.5/weather?lat=37.8267&lon=-122.4233&units=metric&appid=';
 // for adding query as string , after URL use "?key=keyvalue&otherkey=othervalue
-// 'https://ai-weather-by-meteosource.p.rapidapi.com/find_places/f716c09ac8msh817ba35130ca948p18226fjsn75ff562fbe14';
-//'https://visual-crossing-weather.p.rapidapi.com/forecast?rapidapi-key=9d73ce9cd3msh701b57481cfc70ep13d701jsna54dc026f6e&lat=37.8267&lon=-122.4233';
-//'https://air-quality.p.rapidapi.com/current/airquality?rapidapi-key=9d73ce9cd3msh701b57481cfc70ep13d701jsna54dc026f6e&lat=37.8267&lon=-122.4233';
+// 'https://ai-weather-by-meteosource.p.rapidapi.com/find_places/';
+//'https://visual-crossing-weather.p.rapidapi.com/forecast?rapidapi-key=&lat=37.8267&lon=-122.4233';
+//'https://air-quality.p.rapidapi.com/current/airquality?rapidapi-key=&lat=37.8267&lon=-122.4233';
 //call request function with two argument, 1st is an option object which outline what we'd like to do, thats where we provide the url and other information. 2nd argument is a function to run once we actually have that response.
 request({ url: url, json: true }, (error, response) => {
   // here only one parameter could be true at the same time. Its may be error or may response
